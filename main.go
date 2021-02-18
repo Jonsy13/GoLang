741c4014ef806921b60f66da186fb547c895bff9
Jonsy13 <vedant.shrotria@mayadata.io>
2021-02-18 14:53:58 +0530

    Added manifest.
    Signed-off-by: Jonsy13 <vedant.shrotria@mayadata.io>

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,41 +8,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "homepage Endpoint")
 }
 
-type Articles []Article
-
-func getArticles(w http.ResponseWriter, r *http.Request) {
-
-	articles := Articles{
-		Article{Title: "Jon", Desc: "Haitohai", Content: "Heloooo"},
-	}
-	fmt.Println("Endpoint Hii: All Articles EndPoint")
-	json.NewEncoder(w).Encode(articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "homepage Endpoint hii")
-}
-
-func jonsyPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Jonsy Endpoint hii")
-}
-
-func postArticles(w http.ResponseWriter, r *http.Request) {
+func webhookPayload(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "POST Request Checked")
 }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage).Methods("GET")
-	myRouter.HandleFunc("/jonsy", jonsyPage).Methods("GET")
-	myRouter.HandleFunc("/articles", getArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", postArticles).Methods("POST")
+	myRouter.HandleFunc("/", home).Methods("GET")
+	myRouter.HandleFunc("/image-webhook", webhookPayload).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
